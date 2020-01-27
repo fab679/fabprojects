aa15a000a8a7a7e220ba4c25adc94c18d1d395f6
fabisch <fabisch@localhost.localdomain>
2020-01-27 00:31:59 -0500

    Updates

---
+++ precache-manifest.d02ebc87370249c6a20778a41da0bb3c.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "002bbec40250179dcc78eb80fb897465",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "c133a47f0bee663ded51",
+    "url": "/portfolio/static/css/2.6fcd9522.chunk.css"
+  },
+  {
+    "revision": "2dd4c1b452a9a173a779",
+    "url": "/portfolio/static/css/main.d2a5d8cb.chunk.css"
+  },
+  {
+    "revision": "c133a47f0bee663ded51",
+    "url": "/portfolio/static/js/2.73e74b34.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/portfolio/static/js/2.73e74b34.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2dd4c1b452a9a173a779",
+    "url": "/portfolio/static/js/main.0f708c61.chunk.js"
+  },
+  {
+    "revision": "0d98471f6c3b69ca4251",
+    "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+  },
+  {
+    "revision": "876a8325687358ce46ec26be29118535",
+    "url": "/portfolio/static/media/cool.876a8325.svg"
+  },
+  {
+    "revision": "14e2a71b802416b4058d069c80df0995",
+    "url": "/portfolio/static/media/datascience.14e2a71b.jpg"
+  },
+  {
+    "revision": "38417ea50bbe47ff15e0701473071333",
+    "url": "/portfolio/static/media/django.38417ea5.jpg"
+  },
+  {
+    "revision": "8bd7944b69166068563f1187c8e8b71b",
+    "url": "/portfolio/static/media/fb.8bd7944b.png"
+  },
+  {
+    "revision": "054ab01758a9798dc5dd9b6efc77ab14",
+    "url": "/portfolio/static/media/me.054ab017.jpg"
+  },
+  {
+    "revision": "0707dfb4e220be9f3089db5f5bcb1d33",
+    "url": "/portfolio/static/media/me1.0707dfb4.jpg"
+  },
+  {
+    "revision": "876a8325687358ce46ec26be29118535",
+    "url": "/portfolio/static/media/rce.876a8325.svg"
+  }
+]);
